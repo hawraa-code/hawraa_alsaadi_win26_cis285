cf63cedb964c12c753598ce4b9aefa4758e642af
hawraa-code <hawraals@umich.edu>
2026-04-28 20:03:41 +0000

    add year

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hawraa Get started, edit the page on 4/28.tsx file.
+            Hawraa Get started, edit the page on 4/28/26.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
